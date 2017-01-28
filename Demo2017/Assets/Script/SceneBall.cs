0dbe13e9046876f378e4cdca837abb9b27805d70
PongsiriKasetsin <Pongsiri.kase@bumail.net>
2017-01-28 16:56:51 +0700

    Ball Demo2017

---
+++ Demo2017/Assets/Script/SceneBall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneBall : MonoBehaviour {
+	public Text txtScore;
+	public Text txtAttack;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	int score = 0;
+	int attack = 10;
+	public void DoPushButton(){
+		score++;
+		attack++;
+		txtScore.text = "Score :" + score.ToString ();
+		txtAttack.text = "Attack :" + attack.ToString ();
+	}
+
+	public void DochangeScene(){
+		SceneManager.LoadScene(1);
+		}
+
+}
