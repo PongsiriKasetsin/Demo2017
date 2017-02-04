7c2f9f59069819e5e63024ad4ce8e416c6239c98
PongsiriKasetsin <pongsiri.kase@bumail.net>
2017-02-04 16:39:20 +0700

    Week4

---
+++ Demo2017/Assets/Script/BallController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	public Rigidbody rg;
+	public float speed;
+	public TextMesh txtBall;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		
+	}
+
+	public void Dojump(){
+		rg.velocity = new Vector3 (0, 5, 0);
+	}
+
+	int countCollosion;
+	void OnCollisionEnter(Collision collision) {
+		Debug.LogFormat ("collision is (0)",collision.gameObject.tag);
+		if (collision.gameObject.tag == "Floor") {
+			countCollosion++;
+			txtBall.text = "Floor : " + countCollosion.ToString ();
+		}
+		else if(collision.gameObject.tag == "ceil")
+			Destroy(collision.gameObject);
+	
+	}	
+}
---
+++ Demo2017/Assets/Script/CubeMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+	public Rigidbody rg;
+	public float velocity;
+
+
+	void Start () {
+		rg.velocity = new Vector3 (velocity, 0, 0); //
+		//rg.AddForce(-1,0,0);
+	}
+
+
+	void Update () {
+		
+	}
+}
